4e461cd9fa7c937a5d0a61740a0990b495d74a45
Nathan Patnam <patnamnv@email.wofford.edu>
2019-08-03 23:11:05 -0400

    format all other documents using rules

--- tests/integration/components/nav-bar-test.js
+++ tests/integration/components/nav-bar-test.js
@@ -1,24 +1,23 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import hbs from "htmlbars-inline-precompile";
 
-module('Integration | Component | nav-bar',  function (hooks) {
+module("Integration | Component | nav-bar", function(hooks) {
   setupRenderingTest(hooks);
 
-  hooks.beforeEach(async function () {
+  hooks.beforeEach(async function() {
     await render(hbs`<NavBar />`);
   });
 
-  test('it renders', async function (assert) {
-    assert.dom('[data-test-nav-bar]').exists();
+  test("it renders", async function(assert) {
+    assert.dom("[data-test-nav-bar]").exists();
   });
 
-
-  test('proper text rendered ', async function (assert) {
-    const navbarHeadings = "ABOUT ME EXPERIENCE Logo Placeholder SERVICES PROJECTS"
-    assert.dom('[data-test-nav-bar] div').exists({ count: 5 });
-    assert.dom('[data-test-nav-bar]').includesText(navbarHeadings);
+  test("proper text rendered ", async function(assert) {
+    const navbarHeadings =
+      "ABOUT ME EXPERIENCE Logo Placeholder SERVICES PROJECTS";
+    assert.dom("[data-test-nav-bar] div").exists({ count: 5 });
+    assert.dom("[data-test-nav-bar]").includesText(navbarHeadings);
   });
-
 });
