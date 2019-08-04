7b456f96aeb92ee5696e089c84372e5ae3ef7799
Nathan Patnam <patnamnv@email.wofford.edu>
2019-08-03 23:43:18 -0400

    Update linting file to use update to do date emac version, look at more files, and don't use recommended rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,28 +21,23 @@ module.exports = {
         "blueprints/*/index.js",
         "config/**/*.js",
         "lib/*/index.js",
-        "server/**/*.js"
+        "server/**/*.js",
+        "tests/helpers/**/*.js",
+        "tests/integration/**/*.js",
+        "tests/unit/**/*.js"
       ],
       parserOptions: {
-        sourceType: "script",
-        ecmaVersion: 2015
+        sourceType: "module"
       },
       env: {
         browser: false,
-        node: true
+        node: true,
+        es6: true
       },
       plugins: ["node"],
-      rules: Object.assign(
-        {},
-        require("eslint-plugin-node").configs.recommended.rules,
-        {
-          // add your custom rules and overrides for node files here
-
-          // this can be removed once the following is fixed
-          // https://github.com/mysticatea/eslint-plugin-node/issues/77
-          "node/no-unpublished-require": "off"
-        }
-      )
+      rules: {
+        "node/no-unpublished-require": "off"
+      }
     }
   ]
 };
