2bbe053b67a2ae6acef8e3755914b0bc1c3563f0
Nathan Patnam <patnamnv@email.wofford.edu>
2019-08-08 21:07:06 -0700

    Generalize acceptance test name and add being able to go to the about-me page

--- tests/acceptance/visit-pages-test.js
+++ tests/acceptance/visit-pages-test.js
@@ -2,10 +2,17 @@ import { module, test } from "qunit";
 import { visit, click, currentURL } from "@ember/test-helpers";
 import { setupApplicationTest } from "ember-qunit";
 
-module("Acceptance | visit experience page", function(hooks) {
+module("Acceptance | visit pages", function(hooks) {
   setupApplicationTest(hooks);
 
-  test("visiting /visit-experience-page", async function(assert) {
+  test("visiting about me page", async function(assert) {
+    await visit("/");
+    assert.dom("[data-test-about-me-heading]").exists();
+    await click("[data-test-about-me-heading] span");
+    assert.equal(currentURL(), "/about-me");
+  });
+
+  test("visting experience page", async function(assert) {
     await visit("/");
     assert.dom("[data-test-experience-heading]").exists();
     await click("[data-test-experience-heading] span");
