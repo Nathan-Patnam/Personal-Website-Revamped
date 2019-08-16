f76c26984dff4966347986c74ad35c4eb69505d4
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-16 08:37:58 -0400

    Add tags to each project and make 2 only projects show on medium screens

--- app/data/projects.js
+++ app/data/projects.js
@@ -4,7 +4,8 @@ export default {
             imagePath: './img/projects/facebook-clone.png',
             imageAltDescription: 'Profile page that resembles Facebook UI',
             title: 'Facebook clone',
-            description: 'Created Facebook UI Skeleton using HTML and CSS',
+            description: 'Created Facebook UI Skeleton',
+            tags: ['HTML', 'CSS'],
             viewLink: 'https://hardcore-leavitt-747d51.netlify.com/',
             githubLink: 'https://github.com/Nathan-Patnam/facebook-clone',
             dataCreated: 'March 2018'
@@ -15,7 +16,15 @@ export default {
                 'Final Presentation for Capstone Class where I created this Java package',
             title: 'Java Argument Parser',
             description:
-                'Java package that parses arguments from the command line. Created during Wofford’s Computer Science Capstone Class with three other people.Used Java, Jenkins, Gradle, JUnit, Robot Framework, and Coveralls.',
+                'Java package that parses arguments from the command line. Created during Wofford’s Computer Science Capstone Class with three other people.',
+            tags: [
+                'Java',
+                'Gradle',
+                'Travis',
+                'JUnit',
+                'Robot Framework',
+                'Coveralls.io'
+            ],
             githubLink:
                 'https://github.com/WoffordCollege-COSC410/project-todo',
             liveWebsiteLink: null,
