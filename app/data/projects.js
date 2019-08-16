61e1d22f5eef095ed1aa17743914e514ad1a1ca6
Nathan Patnam <patnamnv@email.wofford.edu>
2019-08-15 23:43:42 -0400

    Add facebook clone project and reformat page

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,6 +1,14 @@
 export default {
   data: [
     {
+      title: "Facebook clone",
+      description: "Created Facebook UI Skeleton using HTML and CSS",
+      viewLink: "https://hardcore-leavitt-747d51.netlify.com/",
+      githubLink: "https://github.com/Nathan-Patnam/facebook-clone",
+      liveWebsiteLink: null,
+      dataCreated: "March 2018"
+    },
+    {
       title: "Java Argument Parser",
       description:
         "Java package that parses arguments from the command line. Created during Wofford’s Computer Science Capstone Class with three other people.Used Java, Jenkins, Gradle, JUnit, Robot Framework, and Coveralls.",
