89c3a51f46036ce35d55f7a1b96dafdee51cce67
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-16 06:53:59 -0400

    format json

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,20 +1,20 @@
 export default {
-  data: [
-    {
-      title: "Facebook clone",
-      description: "Created Facebook UI Skeleton using HTML and CSS",
-      viewLink: "https://hardcore-leavitt-747d51.netlify.com/",
-      githubLink: "https://github.com/Nathan-Patnam/facebook-clone",
-      liveWebsiteLink: null,
-      dataCreated: "March 2018"
-    },
-    {
-      title: "Java Argument Parser",
-      description:
-        "Java package that parses arguments from the command line. Created during Wofford’s Computer Science Capstone Class with three other people.Used Java, Jenkins, Gradle, JUnit, Robot Framework, and Coveralls.",
-      githubLink: "https://github.com/WoffordCollege-COSC410/project-todo",
-      liveWebsiteLink: null,
-      dataCreated: "Fall 2017"
-    }
-  ]
+    data: [
+        {
+            title: 'Facebook clone',
+            description: 'Created Facebook UI Skeleton using HTML and CSS',
+            viewLink: 'https://hardcore-leavitt-747d51.netlify.com/',
+            githubLink: 'https://github.com/Nathan-Patnam/facebook-clone',
+            dataCreated: 'March 2018'
+        },
+        {
+            title: 'Java Argument Parser',
+            description:
+                'Java package that parses arguments from the command line. Created during Wofford’s Computer Science Capstone Class with three other people.Used Java, Jenkins, Gradle, JUnit, Robot Framework, and Coveralls.',
+            githubLink:
+                'https://github.com/WoffordCollege-COSC410/project-todo',
+            liveWebsiteLink: null,
+            dataCreated: 'Fall 2017'
+        }
+    ]
 };
