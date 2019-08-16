b62eed4488a7040714a5c13fb99a5b1ccd0bf846
Nathan Patnam <patnamnv@email.wofford.edu>
2019-08-15 23:16:28 -0400

    Remove data from route and place in controller

---
+++ app/data/projects.js
@@ -0,0 +1,12 @@
+export default {
+  data: [
+    {
+      title: "Java Argument Parser",
+      description:
+        "Java package that parses arguments from the command line. Created during Wofford’s Computer Science Capstone Class with three other people.Used Java, Jenkins, Gradle, JUnit, Robot Framework, and Coveralls.",
+      githubLink: "https://github.com/WoffordCollege-COSC410/project-todo",
+      liveWebsiteLink: null,
+      dataCreated: "Fall 2017"
+    }
+  ]
+};
