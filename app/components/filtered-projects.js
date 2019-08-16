b49ddee4f024dc13ca785485a7bf5234a7fb83d4
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-16 10:15:08 -0400

    Add dummy buttons to filter projects based off type

--- app/components/filtered-projects.js
+++ app/components/filtered-projects.js
@@ -1,8 +1,9 @@
-import Component from "@ember/component";
-import { computed } from "@ember/object";
-import projects from "../data/projects";
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import projects from '../data/projects';
 export default Component.extend({
-  filteredProjects: computed("", function() {
-    return projects.data;
-  })
+    projectsFilterKey: 'ALL',
+    filteredProjects: computed('', function() {
+        return projects.data;
+    })
 });
