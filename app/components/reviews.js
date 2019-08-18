d0472252067734645a010108f323bb21e6a4049e
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-18 19:51:33 -0400

    prototype one review

--- app/components/reviews.js
+++ app/components/reviews.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import reviews from '../data/reviews';
 
 export default Component.extend({
+    tagName: '',
+    foo: computed('reviews', function() {
+        return reviews.data;
+    })
 });
---
+++ app/data/reviews.js
@@ -0,0 +1,12 @@
+export default {
+    data: [
+        {
+            name: 'someone',
+            tagLine: 'Great Tutor',
+            review: 'greqat people',
+            date: 'October 7',
+            stars: '4.5/5',
+            viewLink: 'google.com'
+        }
+    ]
+};
