e8c576158cd1b5adb54501606ac6a22f038802e5
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-19 22:04:44 -0400

    Made reviews smaller, and only give review and no tagline

--- app/data/reviews.js
+++ app/data/reviews.js
@@ -1,29 +1,11 @@
 export default {
     data: [
         {
-            name: 'Joshua',
-            tagLine: 'Knowledgeable and Patient Tutor!',
-            review:
-                'What did I like about his teaching? - I enjoyed how he asked me questions for my understanding, instead of teaching the whole time and assuming that I understood everything. - He had no problem with explaining any topic that I was unfamiliar with, as he taught me about Stacks and Queues. - He sent me resources and practice questions after the lesson, which let me know that he truly wants to invest into my learning. - He asked me, "How do you like my teaching?" after the first hour, and was willing to shift gears regarding the structure of the lesson, if requested.What did he help me accomplish? He helped me to understand the fundamentals of Stacks and how Graph problems work.Now, I am aware of what type of Stack or Graph problems might be asked during technical interviews with popular companies, and how to approach them.The information learned was was invaluable to me! I wish that I had Nathan tutor me a month ago, because it was honestly a very effective lesson, and I plan to have him tutor me frequently.',
-            numberTutoringSession: '5',
-            stars: '5/5',
-            viewLink: 'https://www.wyzant.com/Tutors/TutoringByNathan8'
-        },
-        {
             name: 'Meghana',
             tagLine: 'Knowledgeable and patient',
             review:
-                'Nathan taught me how to sort read lists and get sub lists, and also how to use dictionaries to idea item. With his help I was able to finish off a few problems on the assignment I started.',
+                'Before I had my session with Nathan I didn’t even know if I would be able to answer one question on my practice exam. In the span of two hours he made me understand the reasoning behind 6 papers each having 10 questions. Best tutor ever.',
             numberTutoringSession: '4',
-            stars: '5/5',
-            viewLink: 'https://www.wyzant.com/Tutors/TutoringByNathan8'
-        },
-        {
-            name: 'Jeffrey',
-            tagLine: 'Knowledgeable and Friendly',
-            review:
-                'Nathan taught me how to sort read lists and get sub lists, and also how to use dictionaries to idea item. With his help I was able to finish off a few problems on the assignment I started.',
-            numberTutoringSession: '3',
             stars: '5/5',
             viewLink: 'https://www.wyzant.com/Tutors/TutoringByNathan8'
         },
@@ -31,7 +13,7 @@ export default {
             name: 'Joshua',
             tagLine: 'Processing',
             review:
-                'Nathan did an outstanding job. I was having technical difficulty on my end and he was extremely patient and made every attempt to do whatever he could to help me. He is very knowledgeable and obviously very talented. Now that I’m back up and running, I hope he will continue to help me.',
+                'Nathan did an outstanding job. I was having technical difficulty on my end and he was extremely patient and made every attempt to do whatever he could to help me. He is very knowledgeable and obviously very talented. Now that I’m back up and running,',
             numberTutoringSession: '3',
             stars: '5/5',
             viewLink: 'https://www.wyzant.com/Tutors/TutoringByNathan8'
@@ -50,7 +32,16 @@ export default {
             tagLine:
                 'Great Python tutor with great ideas for how to do better at coding interviews!',
             review:
-                'Nathan not only helped me understand more efficient ways to write code to solve questions, he also made sure I understood the concepts of time and space efficiency and how they relate to writing good programs. He gave me some great tips about how to approach coding interviews, and provided links to YouTube videos that reinforce the topics we talked about and prepare me for further lessons with him.',
+                'Nathan not only helped me understand more efficient ways to write code to solve questions, he also made sure I understood the concepts of time and space efficiency and how they relate to writing good programs.',
+            numberTutoringSession: '1',
+            stars: '5/5',
+            viewLink: 'https://www.wyzant.com/Tutors/TutoringByNathan8'
+        },
+        {
+            name: 'Gerard',
+            tagLine: "Best tutor I've had so far on Wyzant",
+            review:
+                'Nathan is very good at explaining stuff and he will repeat them until students fully understand the material without being annoyed. I would definitely recommend him as a tutor if you are serious about going into computer science field',
             numberTutoringSession: '1',
             stars: '5/5',
             viewLink: 'https://www.wyzant.com/Tutors/TutoringByNathan8'
@@ -65,11 +56,21 @@ export default {
             viewLink: 'https://www.wyzant.com/Tutors/TutoringByNathan8'
         },
         {
-            name: 'Gerard',
-            tagLine: "Best tutor I've had so far on Wyzant",
+            name: 'Jeffrey',
+            tagLine: 'Knowledgeable and Friendly',
             review:
-                "I've had a meeting with Nathan for my Python and I felt like the way he codes is so much more efficient than others I tried on Wyzant.Also, he is very good at explaining stuff and he will repeat them until students fully understand the material without being annoyed.I would definitely recommend him as a tutor if you are serious about going into computer science field and he will be my tutor for life as long as I am in this field.",
-            numberTutoringSession: '1',
+                'Nathan taught me how to sort read lists and get sub lists, and also how to use dictionaries to idea item. With his help I was able to finish off a few problems on the assignment I started.',
+            numberTutoringSession: '3',
+            stars: '5/5',
+            viewLink: 'https://www.wyzant.com/Tutors/TutoringByNathan8'
+        },
+
+        {
+            name: 'Jeff',
+            tagLine: 'Knowledgeable and Friendly',
+            review:
+                'Nathan taught me how to sort read lists and get sub lists, and also how to use dictionaries to idea item. With his help I was able to finish off a few problems on the assignment I started.',
+            numberTutoringSession: '3',
             stars: '5/5',
             viewLink: 'https://www.wyzant.com/Tutors/TutoringByNathan8'
         }
