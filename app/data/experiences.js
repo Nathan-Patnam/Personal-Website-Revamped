bfbe5310b39e3bc6454f5fe068f16633b0720c05
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-21 11:00:44 -0400

    Add content for rest of jobs

--- app/data/experiences.js
+++ app/data/experiences.js
@@ -8,6 +8,51 @@ export default {
             description:
                 "Worked on Heroku's Dashboard team where I am learning Ember, GraphQL, Purple3(CSS library), Heroku, Mirage, and QUnit to build a web- service to triage issues across all of Heroku’s 3400 open source & private GitHub repositories into Salesforce’s Grand Unified System(GUS).",
             viewWork: 'google.com'
+        },
+        {
+            companyName: 'Wyzant',
+            logoPath: 'heroku',
+            title: 'Software Engineering Tutor',
+            date: 'March 2018 - Ongoing',
+            description:
+                'Tutor 23 people online for 85 total hours. Covered topics such as TDD, APIs, OOP, SOLID principles, and unit testing.',
+            viewWork: 'google.com'
+        },
+        {
+            companyName: 'Honda R&D Americas Inc.',
+            logoPath: 'honda',
+            title: 'Full-Stack Mobile Software Engineer Intern',
+            date: 'Fall 2018',
+            description:
+                "Worked on Heroku's Dashboard team where I am learning Ember, GraphQL, Purple3(CSS library), Heroku, Mirage, and QUnit to build a web- service to triage issues across all of Heroku’s 3400 open source & private GitHub repositories into Salesforce’s Grand Unified System(GUS).",
+            viewWork: 'google.com'
+        },
+        {
+            companyName: 'Microsoft',
+            logoPath: 'microsoft',
+            title: 'Data Science Intern',
+            date: 'Summer 2018',
+            description:
+                'Learned an internal query language called SCOPE to parse through 100’s of petabytes of daily telemetry data to identify trends in user interaction with the Windows settings app on Microsoft’s Big Data System called COSMOS.',
+            viewWork: 'google.com'
+        },
+        {
+            companyName: 'UnitedHealth Group',
+            logoPath: 'unitedhealth-group',
+            title: 'Web Developer Intern',
+            date: 'Summer 2019',
+            description:
+                'Redesigned the intern hiring process for software engineering managers by creating a web service that saves ~350 hours of manager time(est.$13, 000 annually) for the Schaumburg, Ill. office.',
+            viewWork: 'google.com'
+        },
+        {
+            companyName: 'Wofford College',
+            logoPath: 'wofford',
+            title: '.NET Developer',
+            date: 'January - October 2017',
+            description:
+                "Worked on Heroku's Dashboard team where I am learning Ember, GraphQL, Purple3(CSS library), Heroku, Mirage, and QUnit to build a web- service to triage issues across all of Heroku’s 3400 open source & private GitHub repositories into Salesforce’s Grand Unified System(GUS).",
+            viewWork: 'google.com'
         }
     ]
 };
