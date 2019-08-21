1328b01152f35afa046e0a4a8662b285c4b7b621
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-21 11:41:31 -0400

    Create outline for education card

--- app/components/experience-education.js
+++ app/components/experience-education.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import experience from '../data/experiences';
-import education from '../data/experiences';
+import education from '../data/educations';
 export default Component.extend({
     experiences: computed(function() {
         return experience.data;
