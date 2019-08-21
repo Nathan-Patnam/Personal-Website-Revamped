187a1adca035ee60241e3a2030b63e3bde804cf0
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-21 15:06:13 -0400

    Add dataquest information

--- app/data/educations.js
+++ app/data/educations.js
@@ -2,25 +2,30 @@ export default {
     data: [
         {
             institution: 'Wofford College',
+            isLogoSVG: true,
             logoPath: 'wofford',
             date: 'Sept 2015 - Dec 2019',
             achiviements: ['completed']
         },
         {
             institution: 'DataQuest.io',
-            logoPath: 'wofford',
+            isLogoSVG: false,
+            logoPath: 'img/logos/dataquest.jpeg',
+            imageAltDescription: 'dataquest.io logo',
             date: 'Fall 2018 - Ongoing',
             achiviements: ['completed']
         },
 
         {
             institution: 'Free Code Camp',
+            isLogoSVG: true,
             logoPath: 'free-code-camp',
             date: 'Fall 2017 - Spring 2018',
             achiviements: ['completed']
         },
         {
             institution: 'Udemy',
+            isLogoSVG: true,
             logoPath: 'udemy',
             date: 'Ongoing',
             achiviements: ['completed']
