45476cc723874e38ddb2ca932430492dfa10d46e
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-21 10:45:23 -0400

    Refractor experience to iterate through json

--- app/components/experience-education.js
+++ app/components/experience-education.js
@@ -1,4 +1,9 @@
 import Component from '@ember/component';
-
+import { computed } from '@ember/object';
+import experience from '../data/experiences';
 export default Component.extend({
+    experiences: computed(function() {
+        console.log(experience);
+        return experience.data;
+    })
 });
---
+++ app/data/experiences.js
@@ -0,0 +1,13 @@
+export default {
+    data: [
+        {
+            companyName: 'Salesforce~Heroku',
+            logoPath: 'heroku',
+            title: 'Full-Stack Software Engineer Intern',
+            date: 'Summer 2019',
+            description:
+                "Worked on Heroku's Dashboard team where I am learning Ember, GraphQL, Purple3(CSS library), Heroku, Mirage, and QUnit to build a web- service to triage issues across all of Heroku’s 3400 open source & private GitHub repositories into Salesforce’s Grand Unified System(GUS).",
+            viewWork: 'google.com'
+        }
+    ]
+};
