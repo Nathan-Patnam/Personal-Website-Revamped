5d0bf111193032627d5f44a80fcc8e916bd00983
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-23 19:39:56 -0400

    Define breakpoints

---
+++ app/breakpoints.js
@@ -0,0 +1,6 @@
+export default {
+    mobile: '(min-width: 320) and (max-width: 480px)',
+    tablet: '(min-width: 481px) and (max-width: 992px)',
+    desktop: '(min-width: 993px) and (max-width: 1200px)',
+    jumbo: '(min-width: 1201px)'
+};
