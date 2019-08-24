bd323109d117516d7a76c6d23f9c2b5cc18982dc
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-23 23:19:25 -0400

    display bullet point for each education in component

--- tests/integration/components/education-card-description-test.js
+++ tests/integration/components/education-card-description-test.js
@@ -1,26 +1,6 @@
-import { module, test } from 'qunit';
+import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | education-card-description', function(hooks) {
-  setupRenderingTest(hooks);
-
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<EducationCardDescription />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <EducationCardDescription>
-        template block text
-      </EducationCardDescription>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
+    setupRenderingTest(hooks);
 });
