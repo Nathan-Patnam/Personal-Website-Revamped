f763e64448e5269b8f535fef760933abe84ff2f5
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-23 20:04:02 -0400

    refractor how logos are rendered using a each-in loop inside of a component

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,4 +1,9 @@
 import Component from '@ember/component';
-
+import { computed } from '@ember/object';
+import footerLogos from '../data/footer-logos';
 export default Component.extend({
+    tagName: '',
+    mediaLinks: computed(function() {
+        return footerLogos.data;
+    })
 });
---
+++ app/data/footer-logos.js
@@ -0,0 +1,28 @@
+export default {
+    data: [
+        {
+            logoFilePath: 'github-logo',
+            linkToResource: 'https://github.com/Nathan-Patnam'
+        },
+        {
+            logoFilePath: 'youtube-logo',
+            linkToResource: 'https://www.youtube.com/user/vert373'
+        },
+        {
+            logoFilePath: 'linkedin-logo',
+            linkToResource: 'https://www.linkedin.com/in/nathan-patnam/'
+        },
+        {
+            logoFilePath: 'resume-logo',
+            linkToResource: 'https://github.com/Nathan-Patnam'
+        },
+        {
+            logoFilePath: 'github-logo',
+            linkToResource: 'https://github.com/Nathan-Patnam'
+        },
+        {
+            logoFilePath: 'medium-logo',
+            linkToResource: 'https://github.com/Nathan-Patnam'
+        }
+    ]
+};
