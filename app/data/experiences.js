40cecd254396b6161fbf74996ee21ef027a0cfc6
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-25 05:17:34 -0400

    Add location and truncate company/title names

--- app/data/experiences.js
+++ app/data/experiences.js
@@ -1,10 +1,11 @@
 export default {
     data: [
         {
-            companyName: 'Salesforce~Heroku',
+            companyName: 'Heroku',
             logoPath: 'heroku',
-            title: 'Full-Stack Software Engineer Intern',
+            title: 'Full-Stack Web Intern',
             date: 'Summer 2019',
+            location: 'San Francisco, CA & Remote',
             achievements: [
                 "Worked on Heroku's Dashboard team where I am learning Ember, GraphQL, Purple3(CSS library), Heroku, Mirage, and QUnit to build a web- service to triage issues across all of Heroku’s 3400 open source & private GitHub repositories into Salesforce’s Grand Unified System(GUS)."
             ],
@@ -29,6 +30,7 @@ export default {
             logoPath: 'wyzant',
             title: 'Programming Tutor',
             date: 'March 2018 - Ongoing',
+            location: 'Remote',
             achievements: [
                 'Tutor 23 people online for 85 total hours.',
                 'Covered topics such as TDD, APIs, OOP, SOLID principles, and unit testing.'
@@ -40,10 +42,11 @@ export default {
             tech: ['Python', 'Java', 'JavaScript']
         },
         {
-            companyName: 'Honda R&D Americas Inc.',
+            companyName: 'Honda R&D Americas',
             logoPath: 'honda',
-            title: 'Full-Stack Mobile Software Engineer Intern',
+            title: 'Full-Stack Mobile Intern',
             date: 'Fall 2018',
+            location: 'Raymond, OH',
             achievements: [
                 'Learned React Native, Redux, AWS Lambda, and Firebase to create Android and iOS apps to prototype new driving experiences to save our customers time and/or money.'
             ],
@@ -62,6 +65,7 @@ export default {
             logoPath: 'microsoft',
             title: 'Data Science Intern',
             date: 'Summer 2018',
+            location: 'Redmond, WA',
             achievements: [
                 'Learned an internal query language called SCOPE to parse through 100’s of petabytes of daily telemetry data to identify trends in user interaction with the Windows settings app on Microsoft’s Big Data System called COSMOS.'
             ],
@@ -73,6 +77,7 @@ export default {
             logoPath: 'upwork',
             title: 'Freelance Developer',
             date: 'January - April 2018',
+            location: 'Remote',
             achievements: [
                 'Cryptocurrency ROI Calculator with Facebook Sharing Results Feature',
                 'Web form(was a private job for a VR company so their is just a picture of it it below)'
@@ -82,8 +87,9 @@ export default {
         {
             companyName: 'Youtube',
             logoPath: 'youtube',
-            title: 'Programming Tutor Content Creator',
+            title: 'CS Content Creator',
             date: 'January - April 2018',
+            location: 'Remote',
             achievements: [
                 'Creatd a couple of Youtube videos on data structures & algorithms as well as implementing them in Python'
             ],
@@ -96,8 +102,9 @@ export default {
             logoPath: 'unitedhealth-group',
             title: 'Web Developer Intern',
             date: 'Summer 2017',
+            location: 'Schaumburg, IL',
             achievements: [
-                'Redesigned the intern hiring process for software engineering managers by creating a web service that saves ~350 hours of manager time(est.$13, 000 annually) for the Schaumburg, Ill. office.'
+                'Redesigned the intern hiring process for software engineering managers by creating a web service that saves ~350 hours of manager time(est. $13,000 annually) for the Schaumburg, Ill. office.'
             ],
             viewWork: 'google.com',
             tech: ['CSS', 'HTML', 'jQuery', 'Bootstrap', 'JavaScript']
@@ -108,6 +115,7 @@ export default {
             logoPath: 'wofford',
             title: '.NET Developer',
             date: 'January - October 2017',
+            location: 'Spartanbrg, SC',
             achievements: [
                 'Developed 5 web forms in .NET using HTML, CSS, JavaScript, jQuery, Git, VB, and MySQL using Microsoft SQL server.',
                 'Increased branch coverage with better unit tests.',
