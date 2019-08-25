834f41fdbafa3bb57f9c4b66b2e9fd7fd86b8057
Nathan Patnam <nathanpatnam@hotmail.com>
2019-08-25 04:53:51 -0400

    Make bullet points for achivements for each job

--- app/data/experiences.js
+++ app/data/experiences.js
@@ -5,8 +5,10 @@ export default {
             logoPath: 'heroku',
             title: 'Full-Stack Software Engineer Intern',
             date: 'Summer 2019',
-            description:
-                "Worked on Heroku's Dashboard team where I am learning Ember, GraphQL, Purple3(CSS library), Heroku, Mirage, and QUnit to build a web- service to triage issues across all of Heroku’s 3400 open source & private GitHub repositories into Salesforce’s Grand Unified System(GUS).",
+            achievements: [
+                "Worked on Heroku's Dashboard team where I am learning Ember, GraphQL, Purple3(CSS library), Heroku, Mirage, and QUnit to build a web- service to triage issues across all of Heroku’s 3400 open source & private GitHub repositories into Salesforce’s Grand Unified System(GUS)."
+            ],
+
             viewWork: 'google.com',
             viewAbleContent: 'View Demo',
             viewAbleContentLink: 'https://youtu.be/M_qvAdgDZ38',
@@ -25,10 +27,13 @@ export default {
         {
             companyName: 'Wyzant',
             logoPath: 'wyzant',
-            title: 'Software Engineering Tutor',
+            title: 'Programming Tutor',
             date: 'March 2018 - Ongoing',
-            description:
-                'Tutor 23 people online for 85 total hours. Covered topics such as TDD, APIs, OOP, SOLID principles, and unit testing.',
+            achievements: [
+                'Tutor 23 people online for 85 total hours.',
+                'Covered topics such as TDD, APIs, OOP, SOLID principles, and unit testing.'
+            ],
+
             viewAbleContent: 'View Tutoring Profile',
             viewAbleContentLink:
                 'https://www.wyzant.com/Tutors/TutoringByNathan8',
@@ -39,8 +44,10 @@ export default {
             logoPath: 'honda',
             title: 'Full-Stack Mobile Software Engineer Intern',
             date: 'Fall 2018',
-            description:
-                'Learned React Native, Redux, AWS Lambda, and Firebase to create Android and iOS apps to prototype new driving experiences to save our customers time and/or money.',
+            achievements: [
+                'Learned React Native, Redux, AWS Lambda, and Firebase to create Android and iOS apps to prototype new driving experiences to save our customers time and/or money.'
+            ],
+
             viewWork: 'google.com',
             tech: [
                 'React Native',
@@ -55,49 +62,57 @@ export default {
             logoPath: 'microsoft',
             title: 'Data Science Intern',
             date: 'Summer 2018',
-            description:
-                'Learned an internal query language called SCOPE to parse through 100’s of petabytes of daily telemetry data to identify trends in user interaction with the Windows settings app on Microsoft’s Big Data System called COSMOS.',
+            achievements: [
+                'Learned an internal query language called SCOPE to parse through 100’s of petabytes of daily telemetry data to identify trends in user interaction with the Windows settings app on Microsoft’s Big Data System called COSMOS.'
+            ],
             viewWork: 'google.com',
             tech: ['Power BI', 'C#', 'Azure', 'SCOPE & MySQL']
-        },
-        {
-            companyName: 'UnitedHealth Group',
-            logoPath: 'unitedhealth-group',
-            title: 'Web Developer Intern',
-            date: 'Summer 2019',
-            description:
-                'Redesigned the intern hiring process for software engineering managers by creating a web service that saves ~350 hours of manager time(est.$13, 000 annually) for the Schaumburg, Ill. office.',
-            viewWork: 'google.com',
-            tech: ['CSS', 'HTML', 'jQuery', 'Bootstrap', 'JavaScript']
         },
         {
             companyName: 'Upwork',
             logoPath: 'upwork',
             title: 'Freelance Developer',
             date: 'January - April 2018',
-            description:
-                'Finished Projects:\n - Cryptocurrency ROI Calculator with Facebook Sharing Results Feature\n- Web form(was a private job for a VR company so their is just a picture of it it below)',
-            tech: ['Python', 'JavaScript', 'HTML', 'CSS']
+            achievements: [
+                'Cryptocurrency ROI Calculator with Facebook Sharing Results Feature',
+                'Web form(was a private job for a VR company so their is just a picture of it it below)'
+            ],
+            tech: ['Python', 'JavaScript', 'HTML', 'CSS', 'Boostrap']
         },
-
         {
             companyName: 'Youtube',
             logoPath: 'youtube',
             title: 'Programming Tutor Content Creator',
             date: 'January - April 2018',
-            description:
-                'Creatd a couple of Youtube videos on data structures & algorithms as well as implementing them in Python',
+            achievements: [
+                'Creatd a couple of Youtube videos on data structures & algorithms as well as implementing them in Python'
+            ],
             viewAbleContent: 'View Youtube Channel',
             viewAbleContentLink: 'https://www.youtube.com/user/vert373',
             tech: ['Python']
         },
         {
+            companyName: 'UnitedHealth Group',
+            logoPath: 'unitedhealth-group',
+            title: 'Web Developer Intern',
+            date: 'Summer 2017',
+            achievements: [
+                'Redesigned the intern hiring process for software engineering managers by creating a web service that saves ~350 hours of manager time(est.$13, 000 annually) for the Schaumburg, Ill. office.'
+            ],
+            viewWork: 'google.com',
+            tech: ['CSS', 'HTML', 'jQuery', 'Bootstrap', 'JavaScript']
+        },
+
+        {
             companyName: 'Wofford College',
             logoPath: 'wofford',
             title: '.NET Developer',
             date: 'January - October 2017',
-            description:
-                'Developed 5 web forms in .NET using HTML, CSS, JavaScript, jQuery, Git, VB, and MySQL using Microsoft SQL server. Increased branch coverage with better unit tests. Used Ektron, a CMS, to make updates on Wofford’s websites.',
+            achievements: [
+                'Developed 5 web forms in .NET using HTML, CSS, JavaScript, jQuery, Git, VB, and MySQL using Microsoft SQL server.',
+                'Increased branch coverage with better unit tests.',
+                'Used Ektron, a CMS, to make updates on Wofford’s websites.'
+            ],
             viewWork: 'google.com',
             tech: [
                 '.NET',
