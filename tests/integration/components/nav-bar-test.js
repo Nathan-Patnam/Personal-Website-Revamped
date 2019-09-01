dc5c6496a62ac4abb864f371f2536d1fbfc8b946
Nathan-Patnam <nathanpatnam@hotmail.com>
2019-08-31 21:33:14 -0400

    Remove about-me heading and shorten logo placeholder

--- tests/integration/components/nav-bar-test.js
+++ tests/integration/components/nav-bar-test.js
@@ -1,22 +1,22 @@
-import { module, test } from "qunit";
-import { setupRenderingTest } from "ember-qunit";
-import { render } from "@ember/test-helpers";
-import hbs from "htmlbars-inline-precompile";
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
 
-module("Integration | Component | nav-bar", function(hooks) {
-  setupRenderingTest(hooks);
+module('Integration | Component | nav-bar', function(hooks) {
+    setupRenderingTest(hooks);
 
-  hooks.beforeEach(async function() {
-    await render(hbs`<NavBar />`);
-  });
+    hooks.beforeEach(async function() {
+        await render(hbs`<NavBar />`);
+    });
 
-  test("it renders", async function(assert) {
-    assert.dom("[data-test-nav-bar]").exists();
-  });
+    test('it renders', async function(assert) {
+        assert.dom('[data-test-nav-bar]').exists();
+    });
 
-  test("proper text rendered ", async function(assert) {
-    const navbarHeadings = "ABOUT ME EXPERIENCE SERVICES PROJECTS";
-    assert.dom("[data-test-headings] ul li").exists({ count: 4 });
-    assert.dom("[data-test-nav-bar]").includesText(navbarHeadings);
-  });
+    test('proper text rendered ', async function(assert) {
+        const navbarHeadings = 'EXPERIENCE SERVICES PROJECTS';
+        assert.dom('[data-test-headings] ul li').exists({ count: 3 });
+        assert.dom('[data-test-nav-bar]').includesText(navbarHeadings);
+    });
 });
