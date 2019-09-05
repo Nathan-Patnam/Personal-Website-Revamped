132f4ccdefff1b774b0f055d33776b54d0dc2f52
Nathan-Patnam <nathanpatnam@hotmail.com>
2019-09-05 02:06:05 -0400

    update contents

--- app/data/educations.js
+++ app/data/educations.js
@@ -1,22 +1,18 @@
 export default {
     data: [
         {
-            institution: 'Wofford College',
-            isLogoSVG: true,
-            logoPath: 'wofford',
+            companyName: 'Wofford College',
+            desktopLogo: 'wofford',
+            mobileLogo: 'wofford',
+            title: 'Undergraduate Student',
             date: 'Sept 2015 - Dec 2019',
-            achiviements: [
-                {
-                    isCertficate: false,
-                    name:
-                        'Graduating in December 2019 with a B.A. in Computer Science'
-                },
-                {
-                    isCertficate: false,
-                    name: 'GPA : 3.7/4.0 & CS GPA 3.9/4.0'
-                }
+            location: 'Spartanburg, SC',
+            achievements: [
+                'Graduating in December 2019 with a B.A. in Computer Science',
+
+                'GPA : 3.7/4.0 & CS GPA 3.9/4.0'
             ],
-            tags: [
+            tech: [
                 'Python',
                 'Java',
                 'JavaScript',
@@ -28,65 +24,40 @@ export default {
             ]
         },
         {
-            institution: 'DataQuest.io',
-            isLogoSVG: false,
-            logoPath: 'img/logos/dataquest.jpeg',
-            imageAltDescription: 'dataquest.io logo',
+            companyName: 'DataQuest.io',
+            desktopLogo: 'dataquest',
+            mobileLogo: 'dataquest',
+            title: 'Online course',
             date: 'Fall 2018 - Ongoing',
-            achiviements: [
-                {
-                    isCertficate: true,
-                    name: 'Pandas And NumPy Fundamentals Course',
-                    url:
-                        'https://app.dataquest.io/view_cert/H2W2LQBTOPBHYQYEX725/'
-                },
-                {
-                    isCertficate: true,
-                    name: 'Exploratory Data Visualization',
-                    url:
-                        'https://app.dataquest.io/view_cert/6HE79LB3CQVZM4WB0LQ2/'
-                },
-                {
-                    isCertficate: true,
-                    name: 'APIs and Web Scraping',
-                    url:
-                        'https://app.dataquest.io/view_cert/E82OP8WH80CDW9UVDWKM/'
-                },
-                {
-                    isCertficate: true,
-                    name: 'Python Programming: Intermediate',
-                    url:
-                        'https://app.dataquest.io/view_cert/38TBNA2AV7ZC1PJW7RNB/'
-                }
+            location: 'Online',
+            achievements: [
+                'Pandas And NumPy Fundamentals Course',
+                'Exploratory Data Visualization',
+                'APIs and Web Scraping',
+                'Python Programming: Intermediate'
             ],
-            tags: ['Python', 'Pandas & NumPy', 'Git', 'R']
+            tech: ['Python', 'Pandas & NumPy', 'Git', 'R']
         },
 
         {
-            institution: 'Free Code Camp',
-            isLogoSVG: true,
-            logoPath: 'free-code-camp',
+            companyName: 'Free Code Camp',
+            desktopLogo: 'free-code-camp-desktop',
+            mobileLogo: 'free-code-camp-mobile',
+            title: 'Online course',
             date: 'Fall 2017 - Spring 2018',
-            achiviements: [
-                {
-                    isCertficate: false,
-                    name: 'Responsive Web Design Certificate'
-                }
-            ],
-            tags: ['HTML', 'CSS', 'JavaScript']
+            location: 'Online',
+            achievements: ['Responsive Web Design Certificate'],
+            tech: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'BootStrap']
         },
         {
-            institution: 'Udemy',
-            isLogoSVG: true,
-            logoPath: 'udemy',
+            companyName: 'Udemy',
+            desktopLogo: 'udemy-desktop',
+            mobileLogo: 'udemy-mobile',
+            title: 'Online course',
             date: 'Fall 2017 - Ongoing',
-            achiviements: [
-                {
-                    isCertficate: false,
-                    name: 'The Ultimate MySQL Bootcamp'
-                }
-            ],
-            tags: ['MySQL']
+            location: 'Online',
+            achievements: ['The Ultimate MySQL Bootcamp'],
+            tech: ['MySQL']
         }
     ]
 };
