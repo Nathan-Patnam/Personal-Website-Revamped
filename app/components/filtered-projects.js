e99376bd7facf2af64c98886e3fdcab1e630239d
Nathan-Patnam <nathanpatnam@hotmail.com>
2019-09-07 00:55:30 -0400

    color filter button that is active

--- app/components/filtered-projects.js
+++ app/components/filtered-projects.js
@@ -3,6 +3,15 @@ import { computed } from '@ember/object';
 import projects from '../data/projects';
 export default Component.extend({
     projectsFilterKey: 'APP',
+    filterOptions: computed(function() {
+        const filtersToKey = {
+            'Web Apps': 'APP',
+            Videos: 'VIDEO',
+            Articles: 'ARTICLE',
+            All: 'ALL'
+        };
+        return filtersToKey;
+    }),
     filteredProjects: computed('projectsFilterKey', function() {
         const data = projects.data;
         const filterKey = this.projectsFilterKey;
