5a074c5d3d5d1ce3622c4890e052143367f29dfc
Nathan-Patnam <nathanpatnam@hotmail.com>
2019-09-07 00:00:10 -0400

    iterate through data representing icons and move over styling

--- app/components/mobile-tab-navbar.js
+++ app/components/mobile-tab-navbar.js
@@ -1,4 +1,8 @@
 import Component from '@ember/component';
-
+import { computed } from '@ember/object';
+import mobileNavIcons from '../data/mobile-nav-icons';
 export default Component.extend({
+    navIcons: computed(function() {
+        return mobileNavIcons.data;
+    })
 });
---
+++ app/data/mobile-nav-icons.js
@@ -0,0 +1,24 @@
+export default {
+    data: [
+        {
+            iconFilePath: 'mobile-about-me',
+            iconName: 'About',
+            route: '/'
+        },
+        {
+            iconFilePath: 'mobile-work',
+            iconName: 'Experience',
+            route: 'experience'
+        },
+        {
+            iconFilePath: 'mobile-services',
+            iconName: 'Services',
+            route: 'services'
+        },
+        {
+            iconFilePath: 'mobile-projects',
+            iconName: 'Projects',
+            route: 'projects'
+        }
+    ]
+};
