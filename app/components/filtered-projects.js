be2b1ff824d39833b0ac2110f304e9367258b2c2
Nathan-Patnam <nathanpatnam@hotmail.com>
2019-09-07 01:11:28 -0400

    randomly sorts cards when filitered

--- app/components/filtered-projects.js
+++ app/components/filtered-projects.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import projects from '../data/projects';
+
 export default Component.extend({
     projectsFilterKey: 'APP',
     filterOptions: computed(function() {
@@ -18,8 +19,25 @@ export default Component.extend({
         if (filterKey === 'ALL') {
             return data;
         }
-        return data.filter(item => {
+        let filteredData = data.filter(item => {
             return item.title, item.type.includes(filterKey);
         });
+
+        let counter = filteredData.length;
+
+        // While there are elements in the array
+        while (counter > 0) {
+            // Pick a random index
+            let index = Math.floor(Math.random() * counter);
+
+            // Decrease counter by 1
+            counter--;
+
+            // And swap the last element with it
+            let temp = filteredData[counter];
+            filteredData[counter] = filteredData[index];
+            filteredData[index] = temp;
+        }
+        return filteredData;
     })
 });
