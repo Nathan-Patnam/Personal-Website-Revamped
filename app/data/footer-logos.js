b63c22d9bf1f938da38f65fe5e4de120cef232e4
Nathan-Patnam <nathanpatnam@hotmail.com>
2019-09-13 01:03:38 -0400

    add alternate text to all svgs on page

--- app/data/footer-logos.js
+++ app/data/footer-logos.js
@@ -2,27 +2,33 @@ export default {
     data: [
         {
             logoFilePath: 'github-logo',
-            linkToResource: 'https://github.com/Nathan-Patnam'
+            linkToResource: 'https://github.com/Nathan-Patnam',
+            altText: 'GitHub logo that redirects to my GitHub Profile'
         },
         {
             logoFilePath: 'youtube-logo',
-            linkToResource: 'https://www.youtube.com/user/vert373'
+            linkToResource: 'https://www.youtube.com/user/vert373',
+            altText: 'Youtube logo that redirects to my Youtube Channel'
         },
         {
             logoFilePath: 'linkedin-logo',
-            linkToResource: 'https://www.linkedin.com/in/nathan-patnam/'
+            linkToResource: 'https://www.linkedin.com/in/nathan-patnam/',
+            altText: 'Linkedin logo that redirects to my Linkedin Profile'
         },
         {
             logoFilePath: 'resume-logo',
-            linkToResource: './files/Nathan_Patnam_Resume_SWE.pdf'
+            linkToResource: './files/Nathan_Patnam_Resume_SWE.pdf',
+            altText: 'Resume logo that redirects to my resume'
         },
         {
             logoFilePath: 'mail-logo',
-            linkToResource: 'mailto:nathanpatnam@hotmail.com'
+            linkToResource: 'mailto:nathanpatnam@hotmail.com',
+            altText: 'Mail logo that will open an email to send to me'
         },
         {
             logoFilePath: 'medium-logo',
-            linkToResource: 'https://medium.com/@nathanpatnam'
+            linkToResource: 'https://medium.com/@nathanpatnam',
+            altText: 'Medium logo that redirects to my Medium Profile'
         }
     ]
 };
